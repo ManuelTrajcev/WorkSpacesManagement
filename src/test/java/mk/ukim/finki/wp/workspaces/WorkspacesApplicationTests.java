00ce464f1d5bc1789aa3fcab5ce2bbc11e504197
ManuelTrajcev <manueltrajcev7@gmail.com>
2025-04-13 21:20:37 +0200

    init

---
+++ src/test/java/mk/ukim/finki/wp/workspaces/WorkspacesApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.wp.workspaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkspacesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
